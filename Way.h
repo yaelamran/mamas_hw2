df1667f94d1a08f36b1f4e6c36ea4ebf1ac480c6
Adam Nice <adam.nice@CSF.TECHNION.AC.IL>
2025-12-04 17:40:08 +0200

    Implement Way class with Line struct and init method

--- Way.h
+++ Way.h
@@ -2,8 +2,52 @@
 
 namespace cache
 {
+	struct Line
+	{
+		int tag;
+		int LRU;
+		bool dirty;
+	};
+
 	class Way
 	{
+	private:
+		Line* data;
+		int size;
 
+	public:
+		Way() : data(nullptr), size(0) {}
+
+		~Way() 
+		{
+			delete[] data;
+		}
+
+		void init(int n)
+		{
+			if (data != nullptr)
+			{
+				delete[] data;
+			}
+			size = n;
+			data = new Line[n];
+			for (int i = 0; i < n; ++i)
+			{
+				data[i].tag = 0;
+				data[i].LRU = 0;
+				data[i].dirty = false;
+			}
+		}
+
+		Line* getData() const
+		{
+			return data;
+		}
+
+		int getSize() const
+		{
+			return size;
+		}
 	};
+	
 }
